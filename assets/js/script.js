47815b61190bb3ce821602848e646d3e36222d6a
zidnaarif <zidna.arif@gmail.com>
2022-08-11 21:45:42 +0700

    add about section & profile card

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,17 @@ const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
-})
+});
+
+//sticky navbar 
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById('navbar');
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky')
+  } else {
+    navbar.classList.remove('sticky');
+  }
+};
