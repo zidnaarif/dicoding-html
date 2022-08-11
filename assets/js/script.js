d0a909d80b4fe956219de6761f1aef1cc4b0b0a3
zidnaarif <zidna.arif@gmail.com>
2022-08-11 23:05:13 +0700

    add footer and fix effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,38 @@ function myFunction() {
     navbar.classList.remove('sticky');
   }
 };
+
+//navbar shadow effect
+$(window).on('scroll load', function() {
+    if ($("#navbar").offset().top > 60) {
+        $(".sticky").addClass("shadow");
+    } else {
+        $(".sticky").removeClass("shadow");
+    }
+});
+
+//scroll effect
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
